ce00994ac9d652ab835098a5038fb58d3032f0a7
GutyFC <111283427+GutyFC@users.noreply.github.com>
2022-10-16 20:40:30 -0300

    k

--- Crud-laravel/resources/views/welcome.blade.php
+++ Crud-laravel/resources/views/welcome.blade.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alunos</title>
 </head>
 <body>
+    <form action="/cadastrar-alunos" method="POST">
+    <label for="">Nome:</label>
+    <input type="text"placeholder="Digite o Nome">
+<br /> <br />
+<label for="">Disciplina</label>
+<input type="text"placeholder="Digite a Disciplina">
+<br /><br />
+<button>Enviar Cadastro</button>
+</form>
     
 </body>
 </html>
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Projeto-01-PI-WEB
-versao 0.1 da pagina, contem o ambiente com todos os scripts inseridos 
+# Nome: Projeto-TI-Web
+## Descrição do projeto: ✍🏼 Criaçao de um sistema web Front-end e Back-end.
+<h1 align="center">
+ 🧠 Fizemos o front-end que é a interface do sistema, que o usuario possa interagir com site, onde esta feito o UI/UX, feito por meio do bootstrap. Pelo laravel foi feito o back-end, faz a operação por traz de todo o sistema, como o banco de dados. 
+<h1 align="center">
+Status Do Projeto: 🚀 Pronto
+<h1 align="center">
+🖌️Autores: Matheus Augusto Mendonça RA:, Gustavo Ferreira Carvalho RA: 22006722
+ 
