2e9dec414aea5cf530fd2adc6dff7b07c15f9099
Matheus Augusto Mendonça <luerfelpm@gmail.com>
2022-10-16 16:22:42 -0300

    0.5

--- CRUD/routes/web.php
+++ CRUD/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Htpp\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/cadastrar-aluno',function(request $aluno)){
+    dd($aluno->all());
+    // debug die
+
+
+})
