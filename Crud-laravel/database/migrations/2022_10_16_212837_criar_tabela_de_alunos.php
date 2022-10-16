7686c6a0ef8319ab4b26a419a2f49b85af640696
Matheus Augusto Mendonça <luerfelpm@gmail.com>
2022-10-16 18:41:24 -0300

    0.5

--- Crud-laravel/database/migrations/2022_10_16_212837_criar_tabela_de_alunos.php
+++ Crud-laravel/database/migrations/2022_10_16_212837_criar_tabela_de_alunos.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('Disciplina');
             $table->timestamps();
         });
+          ''
     }
     /**
      * Reverse the migrations.
